ecb98a42af052166b004591ef9a6b0ca1a9b87f8
fabricioaraujo <faa@ufpa.br>
2023-03-16 08:48:43 -0300

    Create q2.py

---
+++ aula4/resolucao_lista_1/q2.py
@@ -0,0 +1,6 @@
+"""
+2.	Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+"""
+numero = input("Digite um número: ")
+
+print("O número informado foi "+numero)
