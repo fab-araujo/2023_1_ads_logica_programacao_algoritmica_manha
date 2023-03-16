a24bdc036b667eb5d4099e2873fafa8c40e3b875
fabricioaraujo <faa@ufpa.br>
2023-03-16 09:26:49 -0300

    Create q6.py

---
+++ aula4/resolucao_lista_1/q6.py
@@ -0,0 +1,8 @@
+"""
+6.	Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+raio = input("Digite o valor raio: ")
+raio = int(raio)
+pi = 3.14
+area = pi * (raio ** 2)
+print(area)
