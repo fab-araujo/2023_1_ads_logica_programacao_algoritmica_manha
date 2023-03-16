5bf4c40d07f035cc8c5accc7008ec66d6ee70b73
fabricioaraujo <faa@ufpa.br>
2023-03-16 08:48:47 -0300

    Create q3.py

---
+++ aula4/resolucao_lista_1/q3.py
@@ -0,0 +1,11 @@
+"""
+3.	Faça um Programa que peça dois números e imprima a soma.
+"""
+x = input("Digite o primeiro número: ")
+y = input("Digite o segundo número: ")
+
+x = int(x)
+y = int(y)
+soma = x + y
+
+print(soma)
