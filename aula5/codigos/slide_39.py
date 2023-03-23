9ef7989e60ac39ac69b8e061fc56a9b175b2c61e
fabricioaraujo <faa@ufpa.br>
2023-03-23 12:21:15 -0300

    Correção slide 39

--- aula5/codigos/slide_39.py
+++ aula5/codigos/slide_39.py
@@ -6,7 +6,7 @@ n2 = float(n2)
 
 if n1 > n2:
     print(str(n1)+" é maior que "+str(n2))
-elif n1 == 1:
+elif n1 == n2:
     print(str(n1)+" é igual "+str(n2))
 else:
     print(str(n2)+" é maior que "+str(n1))
